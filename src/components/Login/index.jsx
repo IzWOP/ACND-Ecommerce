2e47528789646dbaafcb000f92778c7e2b17acaf
Isaac Vazquez <izzyinjurious@aim.com>
2020-12-01 17:14:16 -0800

    placeholder

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -13,10 +13,10 @@ const SignUp =(props)=>{
     {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
     <form onSubmit={handleSubmit(onSubmit)}>
     {/* register your input into the hook by invoking the "register" function */}
-      <input name="email" defaultValue="Email" ref={register} />
+      <input name="email" placeholder="Email" ref={register} />
       
       {/* include validation with required or other standard HTML validation rules */}
-      <input name="password" defaultValue="Password" ref={register({ required: true })} />
+      <input name="password" placeholder="Password" ref={register({ required: true })} />
       {/* errors will return when field validation fails  */}
       {errors.password && <span>This field is required</span>}
       
@@ -27,7 +27,7 @@ const SignUp =(props)=>{
 
 
  const Login = (props) =>{
-
+console.log(props);
   return <section className='login'>
       <SignUp/>
   </section>
