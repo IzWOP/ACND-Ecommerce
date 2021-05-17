248e44bd26970c51f3bb055f202f17e1f2ee3307
Isaac Vazquez <izzyinjurious@aim.com>
2021-05-16 19:42:57 -0500

    added mailchimp tracing

--- src/components/Login/Confirmation.jsx
+++ src/components/Login/Confirmation.jsx
@@ -4,10 +4,7 @@ import {Link} from 'react-router-dom';
 const Confirmation = (props) => {
 
     return <section className="confirmation">
-
         <h2>Con-Fuckin-Firmed</h2>
-
-
     </section>
 }
 
