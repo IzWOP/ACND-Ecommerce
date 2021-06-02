8d101705832413c5dfe4628724dd76a5aff11753
Isaac Vazquez <izzyinjurious@aim.com>
2021-06-02 09:25:52 -0600

    added rest API, Subscribe form.

--- src/components/Home /index.jsx
+++ src/components/Home /index.jsx
@@ -1,59 +1,113 @@
-import React, {useState} from 'react';
-import Amplify, { Auth } from 'aws-amplify';
-
+import React from 'react';
+// import {API} from 'aws-amplify';
+import {useForm} from "react-hook-form";
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+// import '../../utils/api-config';
 
 //stylesheets
 import './index.scss';
-// 
 
-fetch('https://uxh7cpzimd.execute-api.us-west-2.amazonaws.com/subscribe',{
-    method: 'GET',
-    headers: new Headers({'Content-Type': 'text/xml','Content-Type': 'application/json;charset=utf-8',})
-})
-.then(response=>response.json()).then(data=>console.log(data));
+const emailSchema = yup
+    .object()
+    .shape({
+        email: yup
+            .string()
+            .email()
+            .required('Email is required')
+    });
 
-let email = 'izzyinjurous@aim.com'; 
-function subscribeToList(email) {
+// function sendData(email) {
+//     const apiName = 'subscribeAPI';
+//     const path = '/subscribe';
+//     const myInit = {
+//         body: {
+//             email_address: email
+//         }, // replace this with attributes you need
+//         headers: {}, // OPTIONAL
+//     }; //replace this with the path you have configured on your API
 
-  
-    // invoking the actual Lambda function
-    // var lambda = new AWS.Lambda();
-    // lambda.invoke({
-    //   FunctionName: 'mailchimpSubscribe', // your function name
-    //   Payload: JSON.stringify({
-    //     "email": email
-    //   }) // how you send the email to the Lambda event
-    // }, function(err, data){
-    //   if (err) { 
-    //     console.log(err, err.stack);
-    //   } else {
-    //     console.log('Thanks! Your signed up to my email list on Mailchimp');
-    //   }
-    // });
-  }
-subscribeToList(email)
+//     return API
+//         .post(apiName, path, myInit)
+//         .then(response => {
+//             console.log(response);
+//         });
+// }
 
 const Home = () => {
-    const Istate = {
-        user: false,
-        setUser: null
-    };
-    const [user,
-        setUser] = useState(Istate);
-		if (user===true) {
-			console.log('user');
-			console.log(setUser);
-		}
+    // const Istate = {
+    //     user: false,
+    //     setUser: null
+    // };
+    // const [user,
+    //     setUser] = useState(Istate);
+    // if (user === true) {
+    //     console.log('user');
+    //     console.log(setUser);
+    // }
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(emailSchema)
+      }); useForm({resolver: yupResolver(emailSchema)});
+
+      const formFix = (formData) => {
+        const cleanData = {
+            'email': formData.email,
+            'attributes': {
+                email: formData.email,
+            }
+        }
+        return cleanData
+    }
+
+    const onSubmitEmail = (formData) => {
+        // formData.preventDefault()
+        const sendData = formFix(formData)
+        console.log(sendData);
+        let button = document.querySelector('.submit');
+       button.classList.add('clicked')
+       validate()
+       function validate() {
+        setTimeout(function () {
+         button.classList.remove("clicked");
+          button.classList.add("validate");
+          callback()
+        }, 2250);
+        }
+        function callback(){
+            setTimeout(function () {
+                    button.classList.remove("validate");
+            }, 2250);
+        }
+
+    }
+
     return <div className='billboard'>
         <div className='background'></div>
         <div className='container'>
             <div className='row'>
                 <div className='billboard_textbox'>
                     <h1>ACND</h1>
-                    <h2 data-aos="zoom-in" data-aos-delay="3000" data-aos-duration="2000" data-aos-easing="ease-out-cubic">Coming Soon</h2>
-                    <p data-aos="fade-up" data-aos-delay="500" data-aos-duration="1500" data-aos-easing="ease-out-cubic">
+                    <h2
+                        data-aos="zoom-in"
+                        data-aos-delay="3000"
+                        data-aos-duration="2000"
+                        data-aos-easing="ease-out-cubic">Coming Soon</h2>
+                    <p
+                        data-aos="fade-up"
+                        data-aos-delay="500"
+                        data-aos-duration="1500"
+                        data-aos-easing="ease-out-cubic">
                         A lifestyle and clothing brand to help you ascend.
                     </p>
+                    <form onSubmit={handleSubmit(onSubmitEmail)} data-aos-delay="3000"  data-aos="zoom-in" data-aos-duration="1500" data-aos-easing="ease-out-cubic">
+                        <input
+                            className='form-item'
+                            name="email"
+                            placeholder="Email"
+                            {...register("email",{pattern:"[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"})} /> 
+                            {errors.email && <p>{errors.email.message}</p>}
+                        <button className='submit' type="submit" value="Submitt"></button>
+                    </form>
                 </div>
             </div>
         </div>
--- src/utils/api-config.js
+++ src/utils/api-config.js
@@ -1,19 +1,27 @@
-import Amplify from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from '../aws-exports';
 
+Amplify.configure(awsconfig);
 Amplify.configure({
-    // OPTIONAL - if your API requires authentication 
-    Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-west-2:1f77082c-921c-4aa0-aeed-961d11150ea8',
-        // REQUIRED - Amazon Cognito Region
-        region: 'us-west-2',
-    },
     API: {
         endpoints: [
             {
-                name: "mailchimpSubscribe",
-                endpoint: "https://uxh7cpzimd.execute-api.us-west-2.amazonaws.com"
+                name: "mailchimpLambda",
+                endpoint: "https://lambda.us-west-2.amazonaws.com/2015-03-31/functions/mailchipSubscribe/invocations",
+                service: "lambda",
+                region: "us-west-2"
+            },
+            {   
+                name: "mailchimpAPI",
+                endpoint: "https://uxh7cpzimd.execute-api.us-west-2.amazonaws.com",
+                // custom_header: async () => { 
+                //     // return { Authorization : 'token' } 
+                //     // Alternatively, with Cognito User Pools use this:
+                //     // return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
+                //     return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+                //   }
             }
         ]
     }
-});
+})
+API.configure(awsconfig);
