600985537c27336d952eae5aa0024c7a7ed7df26
Isaac Vazquez <izzyinjurious@aim.com>
2021-06-16 18:47:50 -0600

    hero finished

--- src/components/Home /index.jsx
+++ src/components/Home /index.jsx
@@ -11,24 +11,25 @@ const Home = () => {
     return <div className='billboard'>
         <div className='background'></div>
         <div className='container'>
-            <div className='row'>
                 <div className='billboard_textbox'>
-                    <h1>ACND</h1>
+                    <h1 data-aos="fade-up"
+                        data-aos-duration="2000"
+                        data-aos-easing="ease-out-cubic">A brand created to <br/>make leaders,<br/> not followers</h1>
                     <h2
-                        data-aos="zoom-in"
-                        data-aos-delay="3000"
+                        data-aos="fade-up"
+                        data-aos-delay="2000"
                         data-aos-duration="2000"
                         data-aos-easing="ease-out-cubic">Coming Soon</h2>
                     <p
                         data-aos="fade-up"
                         data-aos-delay="500"
-                        data-aos-duration="1500"
+                        data-aos-duration="2000"
                         data-aos-easing="ease-out-cubic">
-                        A lifestyle and clothing brand to help you ascend.
+                        A lifestyle and clothing brand that manifests leaders by surrounding them with other seasoned leaders. More than just a community, but friends, brothers, sisters, familia.<br/>
+                        Somos sangre.
                     </p>
 
                 </div>
-            </div>
         </div>
     </div>
 
