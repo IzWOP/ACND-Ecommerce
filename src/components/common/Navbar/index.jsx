abdbdbf877b60d4f31d24dd15c1ab370651dc11a
Isaac Vazquez <izzyinjurious@aim.com>
2021-06-16 20:01:31 -0600

    added nav bar layout before mobile

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
-
+import './index.scss'
 const Navbar = () => {
-return <h1>navbar</h1>
+    return <> 
+        <nav className='navbar'>
+            <div className='logo'>ACND Lifestlye</div>
+            <div className='nav-links'>
+                <ul> Products
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Accessories</li>
+                </ul>
+                <ul> Company
+                    <li>About Us</li>
+                    <li>Contact us</li>
+                    <li>Returns</li>
+                </ul>
+                <ul> User
+                    <li>Account Information</li>
+                    <li>Orders</li>
+                    <li><button>Sign in</button>/ <button>Sign out</button></li>
+                </ul>
+                <button className='cart'>Cart</button>
+                <button>Search</button>
+            </div>
+        </nav> 
+    </>
 }
 
 export default Navbar;
