7a0677a90c52a476a4a989b2b620583a322f06a4
Isaac Vazquez <izzyinjurious@aim.com>
2021-06-26 17:42:56 -0500

    added two sections and css

--- src/components/Home /index.jsx
+++ src/components/Home /index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShippingFast,faMoneyBillWave } from '@fortawesome/free-solid-svg-icons';
 //stylesheets
 import './index.scss';
 
@@ -35,12 +37,12 @@ const Home = () => {
         <div className='main-body'>
             <section className='intro'>
                 <div className="container">
-                    <h3>
+                    <h2>
                         A lifestyle and clothing brand that manifests leaders by surrounding them with
                         other seasoned leaders. More than just a community, but friends, brothers,
                         sisters, familia.<br/>
                         <span>Somos sangre.</span>
-                    </h3>
+                    </h2>
                 </div>
             </section>
             <section className="latest">
@@ -81,14 +83,46 @@ const Home = () => {
                 </div>
             </section>
             <section className="quality">
-                <div className="container">
+                    <div className="text-box">
                     <h3>The Quality of Products: The Shit</h3>
-                </div>
+                    <h6>Yeah it's pretty great quality</h6>
+                    <p>We understand our customer base aren't made of millionaires yet(we aren't either). We have dedicated our resources to assure that the customer gets the best quality for the lowest price. </p>
+                    </div>
+                    <div className="image-box">
+                        <img src="https://static.pullandbear.net/2/photos//2021/I/0/2/p/9247/599/800/9247599800_2_8_8.jpg?t=1624281688830&imwidth=1440" alt="" />
+                    </div>
             </section>
             <section className="val-props">
-                <div className="container">
-                    <h3>three value props in this hoe
-                    </h3>
+            <div className="container">
+                    <div className="val-prop-cont">
+                        <div className="val-prop">
+                            <div className="val-prop-icon">
+                                <img src="https://res.cloudinary.com/izzyhv/image/upload/v1624746430/ecommerce/ACND/hecho-en-mexico-2_c0vlrc.svg" alt=""/>
+                            </div>
+                            <h4>Made in Mexico</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt
+                                dolorum fugit harum quod molestias repellat. Nam quibusdam aliquam aperiam
+                                nostrum corrupti!</p>
+                        </div>
+                        <div className="val-prop">
+                            <div className="val-prop-icon">
+                            <FontAwesomeIcon icon={faMoneyBillWave}/>
+                            </div>
+                            <h4>Bang for your buck</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt
+                                dolorum fugit harum quod molestias repellat. Nam quibusdam aliquam aperiam
+                                nostrum corrupti!</p>
+                        </div>
+                        <div className="val-prop">
+                            <div className="val-prop-icon">
+                            <FontAwesomeIcon icon={faShippingFast}/>
+                            </div>
+                            <h4>Shipping is rapido</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt
+                                dolorum fugit harum quod molestias repellat. Nam quibusdam aliquam aperiam
+                                nostrum corrupti!</p>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section className="cta-products">
